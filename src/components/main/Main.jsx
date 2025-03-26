06de9158b2e1d1b61639e9d8f596f1095f4045c0
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-03-26 13:33:58 +0800

    feat: add basic outline of Main component

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import "../../styles/Main.css"
+
+export default function Main() {
+    return (
+        <>
+            <div className="cv-container">
+                
+            </div>
+        </>
+    )
+}
