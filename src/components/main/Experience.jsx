9fa0992d19acc6b3b87bed244a57b49e8199f3b6
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-04-01 11:57:43 +0800

    feat: add basic Experience component

---
+++ src/components/main/Experience.jsx
@@ -0,0 +1,9 @@
+import "../../styles/Experience.css";
+
+export default function Experience() {
+    return (
+        <div className="experience-container">
+            <h2 className="experience-header">Experience</h2>
+        </div>
+    )
+}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,13 @@
 import "../../styles/Main.css"
 import GeneralInfo from "./General-info"
+import Experience from "./Experience"
 
 export default function Main() {
     return (
         <>
             <div className="cv-container">
                 <GeneralInfo />
+                <Experience />
             </div>
         </>
     )
